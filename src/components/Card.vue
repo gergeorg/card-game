6ab28335326a0a1f22f0ad39e14b54708ec3c94d
gergeorg <georgey.gerasimov@gmail.com>
2025-11-13 21:56:32 +0300

    add api

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,70 +1,80 @@
 <script setup>
-import { ref } from "vue";
-import CancelIcon from "./icons/CancelIcon.vue";
-import SuccessIcon from "./icons/SuccessIcon.vue";
+	import { ref } from 'vue';
+	import CancelIcon from './icons/CancelIcon.vue';
+	import SuccessIcon from './icons/SuccessIcon.vue';
 
-const props = defineProps({
-	word: {
-		type: String,
-		default: "unknown",
-	},
-	translation: {
-		type: String,
-		default: "?",
-	},
-	initialState: {
-		type: String,
-		default: "closed",
-		validator: (v) => ["closed", "opened"].includes(v),
-	},
-	initialStatus: {
-		type: String,
-		default: "pending",
-		validator: (v) => ["pending", "success", "fail"].includes(v),
-	},
-});
+	const props = defineProps({
+		word: {
+			type: String,
+			default: 'unknown',
+		},
+		translation: {
+			type: String,
+			default: '?',
+		},
+		initialState: {
+			type: String,
+			default: 'closed',
+			validator: (v) => ['closed', 'opened'].includes(v),
+		},
+		initialStatus: {
+			type: String,
+			default: 'pending',
+			validator: (v) => ['pending', 'success', 'fail'].includes(v),
+		},
+		num: {
+			type: Number,
+			default: 1,
+		},
+	});
 
-const emit = defineEmits({
-	flipCard: (payload) => typeof payload === "boolean" || payload === undefined,
-	changeStatus: (payload) => typeof payload === "string" && ["success", "fail"].includes(payload),
-});
+	const emit = defineEmits({
+		flipCard: (payload) => typeof payload === 'boolean' || payload === undefined,
+		changeStatus: (payload) => typeof payload === 'string' && ['success', 'fail'].includes(payload),
+	});
 
-const card = ref({
-	word: props.word,
-	translation: props.translation,
-	state: props.initialState,
-	status: props.initialStatus,
-});
+	const text = ref('Перевернуть');
 
-const handleFlipCard = () => {
-	card.value.state = card.value.state === "closed" ? "opened" : "closed";
-	emit("flipCard", card.value.state === "opened");
-};
+	const card = ref({
+		word: props.word,
+		translation: props.translation,
+		state: props.initialState,
+		status: props.initialStatus,
+		num: props.num,
+	});
 
-const handleSuccess = () => {
-	card.value.status = "success";
-	emit("changeStatus", "success");
-};
+	const handleFlipCard = () => {
+		if (card.value.status !== 'pending') return;
 
-const handleFail = () => {
-	card.value.status = "fail";
-	emit("changeStatus", "fail");
-};
+		card.value.state = card.value.state === 'closed' ? 'opened' : 'closed';
+		emit('flipCard', card.value.state === 'opened');
+	};
+
+	const handleSuccess = () => {
+		card.value.status = 'success';
+		emit('changeStatus', 'success');
+		text.value = 'Завершено';
+	};
+
+	const handleFail = () => {
+		card.value.status = 'fail';
+		emit('changeStatus', 'fail');
+		text.value = 'Завершено';
+	};
 </script>
 
 <template>
 	<li class="card" @click="handleFlipCard()">
-		<div class="card-number">01</div>
+		<div class="card-number">{{ num }}</div>
 
 		<SuccessIcon v-if="card.status === 'success'" class="card-icon-top" />
 		<CancelIcon v-else-if="card.status === 'fail'" class="card-icon-top" />
 
 		<div class="card-content">
-			{{ card.state === "closed" ? card.word : card.translation }}
+			{{ card.state === 'closed' ? card.word : card.translation }}
 		</div>
 
-		<div v-if="card.state === 'opened'" class="card-footer">
-			<span class="card-text">Запомнил?</span>
+		<div v-if="card.state === 'opened' && card.status === 'pending'" class="card-footer">
 			<div class="card-buttons">
 				<button class="card-button" @click.stop="handleFail()">
 					<CancelIcon />
@@ -74,112 +84,113 @@ const handleFail = () => {
 				</button>
 			</div>
 		</div>
+
 		<div v-else class="card-footer">
-			<span class="card-text">Перевернуть</span>
+			<span class="card-text">{{ text }}</span>
 		</div>
 	</li>
 </template>
 
 <style scoped>
-.card {
-	width: 250px;
-	height: 376px;
-	background-color: var(--color-white);
-	border-radius: 16px;
-	box-shadow: 0 0 16px 0 rgba(0, 0, 0, 0.1);
-	padding: 28px 19px;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	position: relative;
-	transition: background-color 0.3s ease-in-out;
-	cursor: pointer;
-}
+	.card {
+		width: 250px;
+		height: 376px;
+		background-color: var(--color-white);
+		border-radius: 16px;
+		box-shadow: 0 0 16px 0 rgba(0, 0, 0, 0.1);
+		padding: 28px 19px;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		position: relative;
+		transition: background-color 0.3s ease-in-out;
+		cursor: pointer;
+	}
 
-.card-number {
-	background-color: var(--color-white);
-	position: absolute;
-	top: 20px;
-	left: 35px;
-	font-weight: 400;
-	font-size: 14px;
-	color: var(--color-text-card);
-	transition: background-color 0.3s ease-in-out;
-}
+	.card-number {
+		background-color: var(--color-white);
+		position: absolute;
+		top: 20px;
+		left: 35px;
+		font-weight: 400;
+		font-size: 14px;
+		color: var(--color-text-card);
+		transition: background-color 0.3s ease-in-out;
+	}
 
-.card-icon-top {
-	position: absolute;
-	top: 15px;
-	left: 50%;
-	transform: translateX(-50%);
-	width: 36px;
-	height: 36px;
-	background-color: var(--color-white);
-	transition: background-color 0.3s ease-in-out;
-}
+	.card-icon-top {
+		position: absolute;
+		top: 15px;
+		left: 50%;
+		transform: translateX(-50%);
+		width: 36px;
+		height: 36px;
+		background-color: var(--color-white);
+		transition: background-color 0.3s ease-in-out;
+	}
 
-.card-content {
-	text-align: center;
-	flex-grow: 1;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	font-size: 18px;
-	font-weight: 400;
-	color: var(--color-text-card);
-	border: 1px solid var(--color-stroke);
-	border-radius: 12px;
-	transition: border 0.3s ease-in-out;
-}
+	.card-content {
+		text-align: center;
+		flex-grow: 1;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 18px;
+		font-weight: 400;
+		color: var(--color-text-card);
+		border: 1px solid var(--color-stroke);
+		border-radius: 12px;
+		transition: border 0.3s ease-in-out;
+	}
 
-.card-footer {
-	position: absolute;
-	bottom: 19px;
-	left: 50%;
-	transform: translateX(-50%);
-	font-weight: 700;
-	font-size: 12px;
-	line-height: 150%;
-	letter-spacing: 0.12em;
-	color: var(--color-text);
-	text-transform: uppercase;
-	background-color: var(--color-white);
-	padding: 0 4px;
-	transition: background-color 0.3s ease-in-out;
-}
+	.card-footer {
+		position: absolute;
+		bottom: 19px;
+		left: 50%;
+		transform: translateX(-50%);
+		font-weight: 700;
+		font-size: 12px;
+		line-height: 150%;
+		letter-spacing: 0.12em;
+		color: var(--color-text);
+		text-transform: uppercase;
+		background-color: var(--color-white);
+		padding: 0 4px;
+		transition: background-color 0.3s ease-in-out;
+	}
 
-.card-buttons {
-	display: flex;
-	gap: 32px;
-}
+	.card-buttons {
+		display: flex;
+		gap: 32px;
+	}
 
-.card-button {
-	background-color: transparent;
-	border: none;
-	cursor: pointer;
-}
+	.card-button {
+		background-color: transparent;
+		border: none;
+		cursor: pointer;
+	}
 
-.card-button:hover {
-	transform: scale(1.2);
-}
+	.card-button:hover {
+		transform: scale(1.2);
+	}
 
-.card:hover {
-	background-color: var(--color-stroke);
-}
+	.card:hover {
+		background-color: var(--color-stroke);
+	}
 
-.card:hover .card-number {
-	background-color: var(--color-stroke);
-}
+	.card:hover .card-number {
+		background-color: var(--color-stroke);
+	}
 
-.card:hover .card-content {
-	border: 1px solid var(--color-primary);
-}
+	.card:hover .card-content {
+		border: 1px solid var(--color-primary);
+	}
 
-.card:hover .card-footer {
-	background-color: var(--color-stroke);
-}
+	.card:hover .card-footer {
+		background-color: var(--color-stroke);
+	}
 
-.card:hover .card-icon-top {
-	background-color: var(--color-stroke);
-}
+	.card:hover .card-icon-top {
+		background-color: var(--color-stroke);
+	}
 </style>
