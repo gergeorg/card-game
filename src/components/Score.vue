f2c259a5592069ff2a9d372471c2e3ab89c69c61
gergeorg <georgey.gerasimov@gmail.com>
2025-11-19 23:31:19 +0300

    final

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,38 +1,38 @@
 <script setup>
-import HeartIcon from "./icons/HeartIcon.vue";
-import { ref } from "vue";
+	import HeartIcon from './icons/HeartIcon.vue';
+	import { inject } from 'vue';
 
-const props = defineProps({
-	currentScore: {
-		type: Number,
-		default: 100,
-	},
-});
+	const score = inject('score');
 
-const scoreRef = ref(props.currentScore);
+	// const props = defineProps({
+	// 	currentScore: {
+	// 		type: Number,
+	// 		default: 0,
+	// 	},
+	// });
 </script>
 
 <template>
 	<div class="score">
-		{{ scoreRef }}
+		{{ score }}
 		<HeartIcon />
 	</div>
 </template>
 
 <style scoped>
-.score {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	gap: 10px;
-	max-width: min-content;
-	border-radius: 36px;
-	padding: 12px 24px;
-	font-weight: 700;
-	font-size: 16px;
-	line-height: 1.5;
-	color: var(--color-primary);
-	box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.15);
-	background-color: var(--color-stroke);
-}
+	.score {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 10px;
+		max-width: min-content;
+		border-radius: 36px;
+		padding: 12px 24px;
+		font-weight: 700;
+		font-size: 16px;
+		line-height: 1.5;
+		color: var(--color-primary);
+		box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.15);
+		background-color: var(--color-stroke);
+	}
 </style>
